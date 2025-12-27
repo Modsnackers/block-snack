2cdd703c8ed9be320311d9d978a145308b958f4c
Akaitatsu <20346471+Akaitatsu@users.noreply.github.com>
2025-12-26 22:39:48 -0500

    Added model provider for DataGen

--- src/main/java/com/mindfair/modsnackers/blocksnack/datagen/DataGenerators.java
+++ src/main/java/com/mindfair/modsnackers/blocksnack/datagen/DataGenerators.java
@@ -17,7 +17,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @EventBusSubscriber(modid = BlockSnack.MODID, value = Dist.CLIENT)
-public class ModDataGenerators {
+public class DataGenerators {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent.Client event) {
         DataGenerator generator = event.getGenerator();
