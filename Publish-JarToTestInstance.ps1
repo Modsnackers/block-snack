90a476a564ec04295ea730ba228bb548dd6901f7
Akaitatsu <20346471+Akaitatsu@users.noreply.github.com>
2025-12-29 15:12:57 -0500

    Added script to do all the gradle stuff and copy the jar to a test instance.

---
+++ Publish-JarToTestInstance.ps1
@@ -0,0 +1,9 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [String]
+    $TestInstancePath
+)
+.\gradlew.bat runData
+.\gradlew.bat jar
+Copy-Item -Path ".\build\libs\blocksnack-0.0.1.jar" -Destination $TestInstancePath -Force
