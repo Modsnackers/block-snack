0848de176cf7640d8c5cc097b18db76c1d77109e
Akaitatsu <20346471+Akaitatsu@users.noreply.github.com>
2025-12-29 16:19:09 -0500

    Fixed test script to handle version changes

--- Publish-JarToTestInstance.ps1
+++ Publish-JarToTestInstance.ps1
@@ -8,6 +8,7 @@ Clear-Host
 .\gradlew.bat build
 .\gradlew.bat runData
 .\gradlew.bat jar
-Copy-Item -Path ".\build\libs\blocksnack-neoforge-1.21.10-0.0.1.jar" -Destination $TestInstancePath -Force
+Remove-Item -Path "$TestInstancePath\blocksnack-*.jar"
+Copy-Item -Path ".\build\libs\blocksnack-*.jar" -Destination $TestInstancePath
 Write-Host ""
 Write-Host "Mod file copied to '$TestInstancePath'." -ForegroundColor Green
